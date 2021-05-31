0f0943c0da411695b90e4d34fcfc6e1585665e85
rajthilakam <rajthilakam@gmail.com>
2021-05-31 00:17:00 -0700

    Menu Added

---
+++ Module3_Assignment/script.js
@@ -0,0 +1,14 @@
+document.getElementById("hello").addEventListener("mouseover", function() {
+    document.getElementById("bckimg").style.backgroundImage = "url(css/pizza.jpg)";
+  }, false);
+  document.getElementById("hello").addEventListener("mouseout", function() {
+     document.getElementById("bckimg").style.backgroundImage = "";
+  }, false);
+
+  document.getElementById("hello2").addEventListener("mouseover", function() {
+    document.getElementById("bckimg").style.backgroundImage = "url(css/pencils.jpg)";
+  }, false);
+  document.getElementById("hello2").addEventListener("mouseout", function() {
+     document.getElementById("bckimg").style.backgroundImage = "";
+  }, false);
+
